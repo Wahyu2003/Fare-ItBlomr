f562f728fcfe2a0a3ce1fffc91d564b82b42a5ac
Hafid Wahyudi <124946688+Wahyu2003@users.noreply.github.com>
2025-05-26 11:39:11 +0700

    Update UserFactory.php

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -3,42 +3,54 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
-    protected static ?string $password;
-
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    public function definition()
     {
+        $roles = ['admin', 'siswa', 'guru'];
+        
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'id_user' => $this->faker->unique()->randomNumber(8),
+            'nik' => $this->faker->unique()->numerify('#############'),
+            'nama' => $this->faker->name,
+            'username' => $this->faker->unique()->userName,
+            'password' => Hash::make('12345678'), // Default password
+            'role' => $this->faker->randomElement($roles),
+            'face_encoding' => $this->faker->text(200),
+            'no_hp_siswa' => $this->faker->phoneNumber,
+            'kelas_id' => $this->faker->numberBetween(1, 10),
+            'foto' => 'users/' . $this->faker->image('public/storage/users', 200, 200, null, false),
+            'nama_ortu' => $this->faker->name,
+            'no_hp_ortu' => $this->faker->phoneNumber,
+            'created_at' => now(),
+            'updated_at' => now(),
+            'deleted_at' => null
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
+    public function admin()
     {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
+        return $this->state([
+            'role' => 'admin',
+        ]);
+    }
+
+    public function siswa()
+    {
+        return $this->state([
+            'role' => 'siswa',
+            'kelas_id' => $this->faker->numberBetween(1, 12),
+        ]);
+    }
+
+    public function guru()
+    {
+        return $this->state([
+            'role' => 'guru',
+            'kelas_id' => null,
         ]);
     }
 }
