cbcdfce457313c474ef3fb73fe49ae4ca435507a
Hafid Wahyudi <124946688+Wahyu2003@users.noreply.github.com>
2025-05-26 18:47:15 +0700

    Create KelasFactory.php

---
+++ backend/database/factories/KelasFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class KelasFactory extends Factory
+{
+    public function definition()
+    {
+        return [
+            'nama_kelas' => $this->faker->randomElement(['X IPA 1', 'X IPA 2', 'XI IPS 1']),
+        ];
+    }
+}
